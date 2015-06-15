5ee98cf0d0a5efb2348e983fcfe5e2695b027db8
unknown <James@A55ML2.DOMAIN.local>
2015-06-15 17:24:53 -0500

    only show users with activated profiles (solves exercise 10.2)

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -16,7 +16,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
-        assert_select 'a[href=?]', user_path(user), test: 'delete'
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
     assert_difference 'User.count', -1 do
@@ -28,5 +28,16 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+  
+  test "only show profiles of activated users" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_equal true, user.activated?
+    end
   end
 end
