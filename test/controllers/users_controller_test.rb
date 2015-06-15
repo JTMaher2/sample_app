910685b8639ae1b082166bb6a773a188b49027bc
unknown <James@A55ML2.DOMAIN.local>
2015-06-15 13:08:37 -0500

    Test for admin attribute being non-accessible by non-admins (solves exercise 9.3)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,6 +38,15 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
   
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password: @other_user.password,
+                                            password_confirmation: @other_user.password_confirmation,
+                                            admin: @other_user.admin }
+    assert_not @other_user.reload.admin?
+  end
+  
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
@@ -57,12 +66,4 @@ class UsersControllerTest < ActionController::TestCase
     end
     assert_redirected_to root_url
   end
-  
-  #test "should not allow the admin attribute to be edited via the web" do
-   # log_in_as(@other_user)
-    #assert_not @other_user.admin?
-    #patch :update, id: @other_user, user: { password: @other_user.password,
-     #                                       password_confirmation: @other_user.password_confirmation,
-      #                                      admin: @other_user.admin }
-    #assert_not @other_user.
 end
